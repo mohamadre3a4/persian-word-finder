feb4618789a36d2784ea39248baf15d5e0ca2bf7
Seyed Mohammad Reza Shahsahebi <mr.shahsahebi@gmail.com>
2018-08-30 09:04:49 +0430

    push to github

---
+++ main.py
@@ -0,0 +1,39 @@
+def readFile(file_name):
+    words = []
+    word_list_file = open(file_name)
+    for word in word_list_file:
+        words.append(word)
+    word_list_file.close()
+
+
+def main:
+    readFile('words.txt')
+    n = int(input('how many letters do you have? '))
+    print('write each of them and enter')
+
+    all_letters = []
+    for i in range(n):
+        all_letters.append(input())
+    print(all_letters)
+    letter_number = input('how many letters does your word have?(you can seperate them with "," ')
+    letter_number = letter_number.split(',')
+    m = []
+    for number in letter_number:
+        m.append(int(number))
+    candidate_words = []
+
+    for word in words:
+        word = word.strip()
+        if len(word) in m:
+            status = True
+            for letter in word:
+                if word.count(letter) > 1:
+                    status = False
+                if letter not in all_letters:
+                    status = False
+            if status == True:
+                candidate_words.append(word)
+
+
+    for word in candidate_words:
+        print(word)
